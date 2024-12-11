8dfd2afdb586d066e2efeba4fb0786da30b074af
Ralphy <ralphynm1000@gmail.com>
2024-12-11 17:33:36 +0100

    02 completado

--- Ejercicios/02_Ejercicios_Funcionesymodulos1.py
+++ Ejercicios/02_Ejercicios_Funcionesymodulos1.py
@@ -2,25 +2,20 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Convierte temperaturas entre Celsius y Fahrenheit")
 
-parser.add_argument("--temp", "-t", type=float, help="Temperatura")
-parser.add_argument("--scale", "-s",help ="temperatura",
+parser.add_argument("--temp", "-t", type=float, help="Temperatura", required=True)
+parser.add_argument("--scale", "-s",help ="Escala", required=False,
                     type=str,
-                    choices=["Celcius", "Farenheit"],
-                    default="Celcius", required=False,
+                    choices=["C", "F"],
+                    default="C",
 )
 
 args = parser.parse_args()
-print("Hello")
 
-
-"""
-#### Ejemplo de ejecución:
-<pre>
-$ python3 temperature_converter.py -h
-usage: temperature_converter.py [-h] [--temp TEMP] [--scale {C,F}]
-Conversor de temperatura options:
--h, --help	show this help message and exit
---temp TEMP	Valor de temperatura a convertir.
---scale {C,F}  Escala de la temperatura de entrada (C or F). Opcional - Valor por defecto: C
-
-"""
+if args.scale == 'C': # Pasar de Celsius a Fahrenheit (0 °C × 9 / 5) + 32 = 32 °F
+    celsius_fahrenheit =  args.temp * 9 / 5 + 32
+    print(f"{args.temp} ºC son {celsius_fahrenheit} ºF")
+elif args.scale == 'F': # Pasar de Fahrenheit a Celsiues (32 °F − 32) × 5 / 9 = 0 °C
+    fahrenheit_celsius =  (args.temp  - 32) * 5 / 9
+    print(f"{args.temp} ºF son {fahrenheit_celsius} ºC")
+else:
+    print("Error,Introduzca una escala válida (C - F)")
